fc0f020784302c713ab08d1825c73b6dd6245ed6
Winnie0408 <1196903869@qq.com>
2024-01-24 17:59:34 +0800

    修复 README.md文件中的表格无法正常显示的问题。
    Fixed the issue that the table in the README.md file could not be displayed properly.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.hwinzniej.musichelper"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.0.0"
+        versionCode = 8
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
