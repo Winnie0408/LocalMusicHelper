88b313e8f830e1b2c07e9f0c80085d8d9dd5c8e3
Winnie0408 <1196903869@qq.com>
2024-05-29 11:16:36 +0800

    更新 部分依赖的版本。
    Update some dependent versions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,8 +90,8 @@ dependencies {
     implementation("com.github.maxbruecken:jaudiotagger-android:master")
 
     //Kotlin协程
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     implementation("androidx.activity:activity-ktx:1.9.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
 
